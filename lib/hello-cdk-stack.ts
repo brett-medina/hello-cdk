5866a3a88767e19c67e7b4d0bc967d73c7e6a9aa
Brett Medina <brett.medina@protonmail.com>
2022-10-10 22:09:28 -0500

    dynamodb, kinesis, lambda

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,5 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
+import { aws_kinesis as kinesis, aws_dynamodb as dynamodb, aws_lambda as lambda } from 'aws-cdk-lib';
+import { ManagedPolicy } from 'aws-cdk-lib/aws-iam';
+import { KinesisEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import { Construct } from 'constructs';
+import * as path from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HelloCdkStack extends cdk.Stack {
@@ -7,6 +11,35 @@ export class HelloCdkStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const stream = new kinesis.Stream(this, 'MyFirstStream', {
+      streamName: 'input-stream',
+    });
+
+    const table = new dynamodb.Table(this, 'Table', {
+      tableName: 'kinesisAggs',
+      partitionKey: { name: 'vendorId', type: dynamodb.AttributeType.NUMBER },
+    });
+
+    const fn = new lambda.Function(this, 'MyFunction', {
+      functionName: 'kinesisLambdaConsumer01',
+      runtime: lambda.Runtime.PYTHON_3_9,
+      handler: 'lambda_function.lambda_handler',
+      environment: {
+        dynamoDBTableName: table.tableName
+      },
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
+      timeout: cdk.Duration.minutes(1)
+    });
+
+    fn.addEventSource(new KinesisEventSource(stream, {
+      startingPosition: lambda.StartingPosition.LATEST,
+      batchSize: 1000,
+      maxBatchingWindow: cdk.Duration.seconds(120),
+      retryAttempts: 2,
+      tumblingWindow: cdk.Duration.seconds(30) 
+    }))
+
+    table.grantWriteData(fn);
 
     // example resource
     // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
